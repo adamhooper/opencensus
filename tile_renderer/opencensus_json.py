817d680d916df3d67d94c761d74a9083f0514558
Adam Hooper <adam@adamhooper.com>
2012-01-19 12:54:05 -0500

    Improve JSON encoder

--- tile_renderer/opencensus_json.py
+++ tile_renderer/opencensus_json.py
@@ -5,22 +5,17 @@ import json
 from utf_grid import UTFGrid as _UTFGrid
 from tile_data import TileData as _TileData
 
-class _Encoder(json.JSONEncoder):
-    def __init__(self):
-        super(_Encoder, self).__init__(ensure_ascii=False, check_circular=False, separators=(',', ':'))
-
-    def default(self, obj):
-        if isinstance(obj, _UTFGrid):
-            return { 'grid': obj.grid, 'keys': obj.keys }
-        elif isinstance(obj, _TileData):
-            return { 'type': 'FeatureCollection', 'features': obj.features, 'utfgrids': obj.utfgrids() }
-        else:
-            raise TypeError('Invalid object type for JSON encoding: %r' % obj)
-
-_encoder = _Encoder()
+def _utfgrids_to_object_list(grids):
+    return map(lambda g: { 'grid': g.grid, 'keys': g.keys }, grids)
 
 def encode(obj):
-    return _encoder.encode(obj)
+    if isinstance(obj, _TileData):
+        obj = {
+            'type': 'FeatureCollection',
+            'features': obj.features,
+            'utfgrids': _utfgrids_to_object_list(obj.utfgrids())
+        }
+    return json.dumps(obj, ensure_ascii=False, check_circular=False, separators=(',', ':'))
 
 def _decode_object(obj):
     if 'grid' in obj and 'keys' in obj:
