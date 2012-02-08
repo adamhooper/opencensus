dcfc9d974bbe3788739774702c53d62151a0fc24
Adam Hooper <adam@adamhooper.com>
2012-02-08 18:43:19 -0500

    Make a script to extract statistics.sqlite3

--- tile_renderer/populate_statistics.py
+++ tile_renderer/populate_statistics.py
@@ -10,7 +10,6 @@ import psycopg2
 import psycopg2.extensions
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
-from psycopg2.extras import RealDictCursor
 
 import opencensus_json
 
@@ -52,42 +51,49 @@ if __name__ == '__main__':
     read_cursor = read_db.cursor()
     write_cursor = write_db.cursor()
 
+    write_cursor.execute('PRAGMA synchronous = OFF')
+
     print 'Creating output database...'
     write_cursor.execute('CREATE TABLE region_statistics (region_id INTEGER PRIMARY KEY, statistics BLOB)')
 
     print 'Loading indicators...'
+    sys.stdout.flush()
     read_cursor.execute('SELECT id, name, value_type FROM indicators')
     indicators = {}
     for (id, name, value_type) in read_cursor:
         indicators[id] = Indicator(name, value_type)
 
-    print 'Loading statistics ("." = 1000 stats)...',
+    print 'Loading list of region IDs...'
+    read_cursor.execute('SELECT DISTINCT region_id FROM indicator_region_values')
+    all_region_ids = [ row[0] for row in read_cursor ]
 
-    read_cursor.execute('SELECT region_id, indicator_id, year, value_integer, value_float, note FROM indicator_region_values')
-    regions = {}
-    n = 0
-    for (region_id, indicator_id, year, value_integer, value_float, note) in read_cursor:
-        if region_id not in regions: regions[region_id] = Region()
-        region = regions[region_id]
-        indicator = indicators[indicator_id]
-        data = indicator.row_to_data(value_integer, value_float, note)
-        region.add_year_indicator_data(int(year), indicator, data)
-        n += 1
-        if n % 1000 == 0:
-            print '.',
-    print
+    print 'Loading statistics per region and writing to SQLite ("qrw" = 1,000 regions queried, read and written)'
+    spread = 1000
 
-    print 'Writing statistics ("." = 1000 regions)...',
-    n = 0
-    for region_id in regions:
-        region = regions[region_id]
-        json_statistics = opencensus_json.encode(region.data)
-        json_statistics_z = zlib.compress(json_statistics.encode('utf-8'))
-        write_cursor.execute('INSERT INTO region_statistics (region_id, statistics) VALUES (?, ?)', (region_id, buffer(json_statistics_z)))
-        n += 1
-        if n % 1000 == 0:
-            print '.',
+    for i in xrange(0, len(all_region_ids), spread):
+        region_ids = all_region_ids[i:i+spread]
+
+        read_cursor.execute('SELECT region_id, indicator_id, year, value_integer, value_float, note FROM indicator_region_values WHERE region_id IN (%s)' % (','.join(map(str, region_ids)),))
+        sys.stdout.write('q'); sys.stdout.flush()
+
+        regions = {}
+        for (region_id, indicator_id, year, value_integer, value_float, note) in read_cursor:
+            if region_id not in regions: regions[region_id] = Region()
+            region = regions[region_id]
+            indicator = indicators[indicator_id]
+            data = indicator.row_to_data(value_integer, value_float, note)
+            region.add_year_indicator_data(int(year), indicator, data)
+
+        sys.stdout.write('r'); sys.stdout.flush()
+
+        for region_id in regions:
+            region = regions[region_id]
+            json_statistics = opencensus_json.encode(region.data)
+            json_statistics_z = zlib.compress(json_statistics.encode('utf-8'))
+            write_cursor.execute('INSERT INTO region_statistics (region_id, statistics) VALUES (?, ?)', (region_id, buffer(json_statistics_z)))
             write_db.commit()
+
+        sys.stdout.write('w'); sys.stdout.flush()
     print
 
     write_db.commit()
