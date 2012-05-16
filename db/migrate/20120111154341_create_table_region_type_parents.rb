7d793cb7ba6dbcc128c78ae6bd7a0bb630994c3c
Adam Hooper <adam@adamhooper.com>
2012-05-16 11:50:57 -0400

    Remove migrations because we have them in script/*.sql

--- db/migrate/20120111154341_create_table_region_type_parents.rb
+++
@@ -1,8 +0,0 @@
-class CreateTableRegionTypeParents < ActiveRecord::Migration
-  def change
-    create_table(:region_type_parents) do |t|
-      t.string(:region_type)
-      t.string(:parent_region_type)
-    end
-  end
-end
