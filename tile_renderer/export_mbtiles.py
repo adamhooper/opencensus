9ba54efa14fb3b7a1043aff03044a876082c1126
Adam Hooper <adam@adamhooper.com>
2012-05-22 18:35:09 -0400

    Change export_mbtiles.py to export SQL, not an SQLite file

--- tile_renderer/export_mbtiles.py
+++ tile_renderer/export_mbtiles.py
@@ -1,27 +1,23 @@
 #!/usr/bin/env python
 
-# Dumps the "tiles" table into an sqlite3 database
+# Outputs the "tiles" table as an sqlite3-compatible series of SQL commands
 
-import sqlite3
+import sys
 import zlib
+# Import psycopg2 directly, so we don't cast utf-8 strings to unicode and back
+import psycopg2
 
-import db
+source_dsn = 'dbname=opencensus_dev user=opencensus_dev password=opencensus_dev host=localhost'
 
 if __name__ == '__main__':
-    import sys
+    print 'PRAGMA synchronous = OFF;'
 
-    write_dsn = sys.argv[1]
+    print >> sys.stderr, 'Creating output database...'
 
-    write_db = sqlite3.connect(write_dsn)
+    print 'CREATE TABLE metadata (name VARCHAR(255) NOT NULL, value VARCHAR(255) NOT NULL, PRIMARY KEY (name));'
+    print 'CREATE TABLE tiles (zoom_level INTEGER NOT NULL, tile_row INTEGER NOT NULL, tile_column INTEGER NOT NULL, tile_data BLOB NOT NULL, PRIMARY KEY (zoom_level, tile_row, tile_column));'
 
-    write_cursor = write_db.cursor()
-    write_cursor.execute('PRAGMA synchronous = OFF')
-
-    print 'Creating output database...'
-    write_cursor.execute('CREATE TABLE metadata (name VARCHAR(255) NOT NULL, value VARCHAR(255) NOT NULL, PRIMARY KEY (name))')
-    write_cursor.execute('CREATE TABLE tiles (zoom_level INTEGER NOT NULL, tile_row INTEGER NOT NULL, tile_column INTEGER NOT NULL, tile_data BLOB NOT NULL, PRIMARY KEY (zoom_level, tile_row, tile_column))')
-
-    print 'Writing metadata...'
+    print >> sys.stderr, 'Writing metadata...'
     metadata = [
         ('name', 'Statistics Canada Census regions'),
         ('type', 'overlay'),
@@ -29,24 +25,30 @@ if __name__ == '__main__':
         ('description', 'Vector region data used to present 2011 Census results'),
         ('format', 'geojson')
     ]
-    write_cursor.executemany('INSERT INTO metadata (name, value) VALUES (?, ?)', metadata)
-    write_db.commit()
+    for key, val in metadata:
+        print "INSERT INTO metadata (name, value) VALUES ('%s', '%s');" % (key, val)
 
-    print 'Selecting tiles...'
-    read_cursor = db.connect().cursor('on_server')
-    read_cursor.execute('SELECT * FROM tiles')
+    print >> sys.stderr, 'Selecting tiles...'
+    connection = psycopg2.connect(source_dsn)
+    connection.set_client_encoding('UTF-8')
+    read_cursor = connection.cursor('on_server')
+    read_cursor.execute('SELECT * FROM tiles ORDER BY zoom_level, tile_row, tile_column')
 
-    print 'Copying tiles ("." = 10,000 tiles)...',
-    for (i, row) in enumerate(read_cursor):
-        zoom_level, tile_row, tile_column, json = row
-        tile_data = zlib.compress(json.encode('utf-8'))
-        write_cursor.execute('INSERT INTO tiles (zoom_level, tile_row, tile_column, tile_data) VALUES (?, ?, ?, ?)', (zoom_level, tile_row, tile_column, buffer(tile_data)))
+    print >> sys.stderr, 'Dumping tiles... ("." = 1,000 tiles)',
+    while True:
+        rows = read_cursor.fetchmany(1000)
+        if len(rows) == 0: break
 
-        if i % 10000 == 9999:
-            sys.stdout.write('.')
-            sys.stdout.flush()
-    print
+        for (zoom_level, tile_row, tile_column, json_data) in rows:
+            tile_data = zlib.compress(json_data)
+            tile_data_hex = tile_data.encode('hex')
 
-    print 'Done!'
+            print "INSERT INTO tiles (zoom_level, tile_row, tile_column, tile_data) VALUES (%d, %d, %d, X'%s');" % (zoom_level, tile_row, tile_column, tile_data_hex)
+
+        sys.stderr.write('.')
+        sys.stderr.flush()
+    print >> sys.stderr
+
+    print >> sys.stderr, 'Done!'
     write_db.commit()
 
