bb90834a713d8f278fc22a3dc3d644c8c30d65ba
Adam Hooper <adam@adamhooper.com>
2012-05-28 13:51:35 -0400

    Fix SQL typos

--- script/create-statistics-schema.sql
+++ script/create-statistics-schema.sql
@@ -19,11 +19,11 @@ CREATE TABLE indicators (
   value_type VARCHAR NOT NULL,
   buckets VARCHAR -- NULL when it isn't a mappable stat
 );
-INSERT INTO indicators (key, name, unit, description, value_type, buckets, bucket_colors)
+INSERT INTO indicators (key, name, unit, description, value_type, buckets)
 VALUES
-('pop', 'Population', '', '', 'integer', NULL, NULL),
-('dwe', 'Dwellings', '', '', 'integer', NULL, NULL),
-('occdwe', 'Occupied dwellings', '', '', 'integer', NULL, NULL),
+('pop', 'Population', '', '', 'integer', NULL),
+('dwe', 'Dwellings', '', '', 'integer', NULL),
+('occdwe', 'Occupied dwellings', '', '', 'integer', NULL),
 ('popdens', 'Population density', 'people per km²', '', 'float',
   '[{"max":5,"color":"#edf8fb"},' ||
   '{"max":20,"color":"#ccece6"},' ||
@@ -44,16 +44,16 @@ VALUES
   '{"max":2.5,"color":"#8c96c6","label":"up to 2½"},' ||
   '{"max":3,"color":"#8856a7"},' ||
   '{"color":"#810f7c"}]'),
-('pop2006', 'Population, 2006', '', '', 'integer', NULL, NULL),
+('pop2006', 'Population, 2006', '', '', 'integer', NULL),
 ('gro', 'Population growth', '%', 'How many more or fewer people are in this region', 'float',
   '[{"max":-5,"color":"#d7191c","label":"shrank over 5%"},' ||
   '{"max":0,"color":"#fdae61","label":"shrank"},' ||
   '{"max":4.9999,"color":"#ffffbf","label":"grew under 5%"},' ||
   '{"max":9.9999,"color":"#a6d96a","label":"grew under 10%"},' ||
   '{"color":"#1a9641","label":"grew at least 10%"}]'),
-('agem', 'Population by age, male', '', '', 'string', NULL, NULL),
-('agef', 'Population by age, female', '', '', 'string', NULL, NULL),
-('age', 'Population by age', '', '', 'string', NULL, NULL),
+('agem', 'Population by age, male', '', '', 'string', NULL),
+('agef', 'Population by age, female', '', '', 'string', NULL),
+('age', 'Population by age', '', '', 'string', NULL),
 ('agemedian', 'Median age', '', '', 'float',
   '[{"max":34.999,"color":"#ffffb2","label":"median under 35"},' ||
   '{"max":39.999,"color":"#fecc5c","label":"under 40"},' ||
@@ -65,9 +65,10 @@ VALUES
   '{"max":48.99999,"color":"#fdae61","label":"over 51% female"},' ||
   '{"max":51,"color":"#ffffbf","label":"about even"},' ||
   '{"max":53,"color":"#abd9e9","label":"over 51% male"},' ||
-  '{"color":"#2c7bb6","label":"over 53% male"}]'),
+  '{"color":"#2c7bb6","label":"over 53% male"}]')
+;
 
-dROP TABLE IF EXISTS indicator_region_values;
+DROP TABLE IF EXISTS indicator_region_values;
 CREATE TABLE indicator_region_values (
   region_id INT NOT NULL,
   indicator_id INT NOT NULL,
