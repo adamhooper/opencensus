55ae89b0eee3f636432725c499dae405c8374bdc
Adam Hooper <adam@adamhooper.com>
2012-05-29 12:38:08 -0400

    Handle tile URL in request

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Opencensus::Application.routes.draw do
-  match('/censusfile/tiles/:zoom_level/:tile_column/:tile_row' => 'tiles#show',
+  match('/censusfile/tiles/index.php/:zoom_level/:tile_column/:tile_row' => 'tiles#show',
         :constraints => { :zoom_level => /[0-9]+/, :tile_column => /[-0-9]+/, :tile_row => /[-0-9]+/ })
 
   root(:to => 'map#show')
